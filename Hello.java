0bb55b8bf83479f5214d7842618b45ec050ab0d4
Md. Rejaur Rahman <rezaurewubd@gmail.com>
2018-01-20 18:55:33 +0600

    Added a new method called greetings in Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello{
-    public static void main(String []args){
+    public static void main(String[] args){
         System.out.println("VS code is awesome...");
+
+        greetings(Md. Rejaur Rahman);
+    }
+
+    public static void greetings(String name){
+        System.out.println("Wlcome" + name );
     }
 }
